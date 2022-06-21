9282e4841f82bee0f8cbe9d6745216f2e7156d48
Justin2172021 <justin.busching@gmail.com>
2022-06-21 16:59:10 -0500

    Created Modal DOM/Elements/Styling

---
+++ form-validation.js
@@ -0,0 +1,20 @@
+// DOM for Modal Module
+const DomModalEvents = (() => {
+  const modal = document.getElementById("myModal");
+  const btn = document.getElementById("myBtn");
+  const span = document.querySelector(".close");
+
+  btn.addEventListener("click", function () {
+    modal.style.display = "block";
+  });
+
+  span.addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+})();
